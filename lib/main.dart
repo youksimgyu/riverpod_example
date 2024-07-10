d872488c19f0ab492b951cff8faf919fbf6f1dc1
youksimgyu <dbrtlarb@gmail.com>
2024-07-10 22:17:47 +0900

    http 패키지 활용 - get, post, put, delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   runApp(
@@ -27,14 +30,92 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String api = 'https://jsonplaceholder.typicode.com/posts';
+  String responseText = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-      body: const Center(
-        child: Text('Go to Second Screen'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () async {
+                final response = await http.get(Uri.parse(api));
+                if (response.statusCode == 200) {
+                  setState(() {
+                    responseText = response.body;
+                  });
+                }
+              },
+              child: const Text('http get'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final response = await http.post(
+                  Uri.parse(api),
+                  headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                  },
+                  body: jsonEncode(
+                      {'title': 'foo', 'body': 'bar', 'userId': '1'}),
+                );
+                if (response.statusCode == 201) {
+                  setState(() {
+                    responseText = response.body;
+                  });
+                }
+              },
+              child: const Text('http post'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final response = await http.put(
+                  Uri.parse('$api/1'),
+                  headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                  },
+                  body: jsonEncode({
+                    'id': '1',
+                    'title': 'foo',
+                    'body': 'bar',
+                    'userId': '1'
+                  }),
+                );
+                if (response.statusCode == 200) {
+                  setState(() {
+                    responseText = response.body;
+                  });
+                }
+              },
+              child: const Text('http put'),
+            ),
+            TextButton(
+              onPressed: () async {
+                final response = await http.delete(Uri.parse('$api/1'));
+                if (response.statusCode == 200) {
+                  setState(() {
+                    responseText = response.body;
+                  });
+                }
+              },
+              child: const Text('http delete'),
+            ),
+            const Divider(),
+            Expanded(
+                child: SingleChildScrollView(
+              child: Text(responseText),
+            )),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
